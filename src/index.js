01d49fe190a9a9ca0e8da45db9edb9fae7ebf032
Jurre van Eijk <jurreveijk13@gmail.com>
2024-03-22 12:53:50 +0100

    idk

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import "./styles.css";
 //637d10571b414c779f872124241603 api key for weatherapi.
 //forecast.json?key=" + apiKey +  "&q=id:" + cityID
+
+let cityID = "london";
+
 async function fetchData() {
 	try {
 		const response = await fetch(
-			"http://api.weatherapi.com/v1/forecast.json?key=637d10571b414c779f872124241603&q=id:purmerend"
+			"http://api.weatherapi.com/v1/forecast.json?key=637d10571b414c779f872124241603&q=id:" +
+				cityID
 		);
 		const data = await response.json();
 		console.log(data);
